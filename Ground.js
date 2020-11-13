daaf36d7b8d44b99fb5c389ef63d0b1090e25295
vaishali410 <vaishali.teotia95@gmail.com>
2020-11-13 19:51:59 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,22 +1,31 @@
-class Ground{
-    constructor(x, y, width, height) {
-        var options={
-            isStatic: true,
-            'restitution': 0,
-            'friction': 0,
-            'density': 1
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
+class ground
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
 
-        World.add(world,this.body); 
-    }
-    display(){
-        rectMode(CENTER);
-        fill("black");
-        strokeWeight(3);
-        stroke("white");
-        rect(this.body.position.x, this.body.position.y, this.width, this. height);
-    }    
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
 }
